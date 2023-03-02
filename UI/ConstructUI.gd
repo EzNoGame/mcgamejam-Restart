d7bb324a3788b5f6712e59784fd924da2da07aea
Ed.Zhang <71359408+EzNoGame@users.noreply.github.com>
2023-03-02 14:05:08 -0500

    update

--- UI/ConstructUI.gd
+++ UI/ConstructUI.gd
@@ -6,7 +6,6 @@ var buildMode
 func _process(delta):
 	buildMode = GMManager.buildMode
 	if buildMode == BuildMode.MACHINEBUILD:
-		var rect = Rect2(rect_position, $PopupPanel.rect_min_size)
-		$PopupPanel.popup(rect)
+		visible = true
 	else:
-		$PopupPanel.visible = false
+		visible = false
