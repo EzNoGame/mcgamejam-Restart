8741cd688150be0175d07ac4133f3d0e057509d6
Ed.Zhang <71359408+EzNoGame@users.noreply.github.com>
2023-03-02 21:02:11 -0500

    update

--- Scenes/machines/HumanFactoryMkII.gd
+++ Scenes/machines/HumanFactoryMkII.gd
@@ -6,3 +6,7 @@ func _init():
 	output_type = ResourceType.HUMAN
 	inputs_positions = {ResourceType.HEART_PUMP: Vector2(0,0),ResourceType.ELECTRICAL_ENERGY:Vector2(1,0),ResourceType.ORGANS:Vector2(2,2)}
 	inputs = {ResourceType.HEART_PUMP: false, ResourceType.ELECTRICAL_ENERGY: false, ResourceType.ORGANS: false}
+
+
+func end():
+	Transition.change_scene("res://scenes/cut_scenes/outro.tscn")
--- Scenes/machines/HumanFactoryVer_1.gd
+++ Scenes/machines/HumanFactoryVer_1.gd
@@ -6,6 +6,3 @@ func _init():
 	output_type = ResourceType.HUMAN
 	inputs_positions = {ResourceType.BLOOD: Vector2(1,0),ResourceType.TISSUE:Vector2(2,2),ResourceType.ELECTRICAL_ENERGY:Vector2(0,0)}
 	inputs = {ResourceType.BLOOD: false, ResourceType.TISSUE: false,ResourceType.ELECTRICAL_ENERGY:false}
-
-func end():
-	Transition.change_scene("res://scenes/cut_scenes/outro.tscn")
